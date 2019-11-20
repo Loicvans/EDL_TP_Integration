5b026595f98a719e76aaf62bc097ae92e5e400ad
Loic Vans <loicvans@gmail.com>
2019-11-20 10:47:59 +0100

    in

--- tests/GumballMachineTest.php
+++ tests/GumballMachineTest.php
@@ -54,8 +54,8 @@ class GumballMachineTest extends PHPUnit_Framework_TestCase
     }
     public function testD()
     {
-        $this->gumballMachineInstance->deletet();
-        
+        //$this->gumballMachineInstance->deletet();
+
     }
     public function testAffichageProfAVI()
     {
